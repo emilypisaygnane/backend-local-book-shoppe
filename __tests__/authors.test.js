de26ca5cc888bb3df88a07f1c2c5d92ec89e4f63
Emily Pisaygnane <episaygnane@me.com>
2022-11-08 20:41:06 -0800

    added first test to return a list of authors, id, first and last name

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -1,15 +1,24 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('author routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/authors should return list of all authors in shoppe', async () => {
+    const res = await request(app).get('/authors');
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBe(6);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      first_name: expect.any(String),
+      last_name: expect.any(String),
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
