44b84927b2a9779871e52e7bbdb8eb2800fd924f
Emily Pisaygnane <episaygnane@me.com>
2022-11-14 10:43:15 -0800

    created model for books

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -11,11 +11,11 @@ describe('book routes', () => {
   it('/books should return list of all books in the shoppe', async () => {
     const res = await request(app).get('/books');
     expect(res.status).toBe(200);
-    expect(res..body.length).toBe(10);
+    expect(res.body.length).toBe(10);
     expect(res.body[0]).toEqual({
       id: expect.any(String),
       title: expect.any(String),
-      released: expect.any(String),
+      released: expect.any(Number),
     });
   });
 
